19d5d0021d976196ef7fb8ea21086206844c3f68
r11b <41546928+r11b@users.noreply.github.com>
2024-03-06 02:44:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = 'sih2019-samaritans-apollo'
+    # MONGO_URI = 'mongodb+srv://keyur:keyur123@cluster0-5ay5y.mongodb.net/Cluster0?retryWrites=true'
+    MONGO_URI = 'mongodb+srv://athnus:QWERTYUIOP@apollo-yhrx4.mongodb.net/em?retryWrites=true'
+    # GOOGLEMAPS_KEY = 'AIzaSyC_qEvLL6iREFnrC3y1hQYvCggj7RsgRSw'
